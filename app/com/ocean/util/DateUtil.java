7c71f4e2f950d057c55df0e1d13110aba50a1bf7
zhouxiaojie <zhouxiaojie@zhouxiajiedembp.lan>
2015-07-09 15:58:07 +0800

    dateutil

--- app/com/ocean/util/DateUtil.java
+++ app/com/ocean/util/DateUtil.java
@@ -6,23 +6,23 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DateUtil {
-	private static final DateFormat DEFAULT_FMT=new SimpleDateFormat("yyyy-MM-dd");
 	
-	private static final DateFormat MIN_FMT = new SimpleDateFormat("yyyy-MM-dd HH:mm");
 	
-	private static final DateFormat YYMMDD_FMT=new SimpleDateFormat("yyyyMMdd");
 	
 	public static String DateDefaultFmt(Date date){
+		 DateFormat DEFAULT_FMT=new SimpleDateFormat("yyyy-MM-dd");
         return DEFAULT_FMT.format(date);
 	}	
 	public static String DateYYMMDDFmt(Date date){
+		DateFormat YYMMDD_FMT=new SimpleDateFormat("yyyyMMdd");
         return YYMMDD_FMT.format(date);
 	}	
 	public static String DateMinFmt(Date date){
-        
+		DateFormat MIN_FMT = new SimpleDateFormat("yyyy-MM-dd HH:mm");
         return MIN_FMT.format(date);
 	}
 	public static Date parseMinFmt(String date) throws ParseException{
+		DateFormat MIN_FMT = new SimpleDateFormat("yyyy-MM-dd HH:mm");
 		return MIN_FMT.parse(date);
 	}
 }
